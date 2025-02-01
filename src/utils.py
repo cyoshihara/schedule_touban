8297e97c220095b0d7f1751e590d35d0d013fc7e
chryshr <c.y0sh1hara.jp@gmail.com>
2025-02-01 20:35:32 +0900

    デバッグ

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ def get_secrets():
     try:
         print("Streamlit Cloud で実行しています")
         gcp_creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-        google_genai_creds_info = service_account.Credentials.from_service_account_info(st.secrets["google_genai"])
+        # google_genai_creds_info = service_account.Credentials.from_service_account_info(st.secrets["google_genai"])
     except:
         print("ローカル環境で実行しています")
         with open("./.local/credentials.json") as f:
@@ -20,7 +20,8 @@ def get_secrets():
         gcp_creds = service_account.Credentials.from_service_account_info(gcp_creds_info)
 
         google_genai_creds_info = creds_info["google_genai"]
-    google_genai_api_key = google_genai_creds_info["api_key"]
+    # google_genai_api_key = google_genai_creds_info["api_key"]
+    google_genai_api_key = ""
     return gcp_creds, google_genai_api_key
 
 
