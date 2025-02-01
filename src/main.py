51c68b63569a491ab711fc712716cd31a33965e4
chryshr <c.y0sh1hara.jp@gmail.com>
2025-02-01 18:46:33 +0900

    リファクタリング

--- src/main.py
+++ src/main.py
@@ -1,33 +1,45 @@
 import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import json
+import polars as pl
 
-# ローカル or Streamlit Cloud の判定
-try:
-    print("Streamlit Cloud で実行しています")
-    creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-except:
-    print("ローカル環境で実行しています")
-    with open("credentials.json") as f:
-        creds_info = json.load(f)
-    creds = service_account.Credentials.from_service_account_info(creds_info)
+import const
+import utils
 
-# Google Drive API クライアントを作成
-drive_service = build("drive", "v3", credentials=creds)
 
-# Google Drive のファイル一覧を取得
-def list_drive_files():
-    results = drive_service.files().list(pageSize=10, fields="files(id, name)").execute()
-    files = results.get("files", [])
-    return files
+def main():
+    gcp_creds, google_genai_api_key = utils.get_secrets()
 
-print("📂 Google Drive API テスト")
+    # Google Drive API クライアントを作成
+    drive_service = build("drive", "v3", credentials=gcp_creds)
 
-if st.button("ファイル一覧を取得"):
-    files = list_drive_files()
-    if files:
-        for file in files:
-            st.write(f"📄 {file['name']} (ID: {file['id']})")
-    else:
-        st.write("ファイルが見つかりませんでした")
+    if st.button("ファイル一覧を取得"):
+        files = utils.list_drive_files(drive_service)
+        if files:
+            for file in files:
+                st.write(f"📄 {file['name']} (ID: {file['id']})")
+        else:
+            st.write("ファイルが見つかりませんでした")
+
+    # 📄 schedule-touban-data (ID: 1eIyBtoj9xbwyWU4Ej477ZBxpccv55eY-)
+    # 📄 trn_touban.csv (ID: 1LnQKVcN7-o_WMtLUn_0c6OqKCunstbAa)
+    # 📄 for_test_trn_touban.csv (ID: 13LK7YU7mmvURWxf4cvv3Et9XaaZU6lpu)
+    # 📄 trn_touban_新しいフォーマットで作成中.csv (ID: 1my7qe1HYiSBPXahuxxFTCagVKyDaQ2_k)
+    # 📄 output.csv (ID: 1Q9x6sywZdXwH4_3xHf_Jv9_sdkaemwKz)
+    # 📄 input.csv (ID: 1PyryHjH5Qi6fwkDrIejDUtW68Oc3V28R)
+    # 📄 mst_parent.csv (ID: 1UzI8WWfLes5PIP9999bQUpQfsQjojZSx)
+    # 📄 _intermediate_parent_attr.csv (ID: 1dz4vyutekOoj1JNUHTL60M3ESrmhssCJ)
+    # 📄 trn_touban_org_till_12.csv (ID: 16zOGSkGW7hOWLS7t8Arw8eJIoXLaSFuR)
+    # 📄 mst_day.csv (ID: 1PvoHPZVwVyZknBybzINyZOqacMTkb0LJ)
+
+    with st.spinner('処理中...'):
+        fpath = utils.download_csv_file(
+            drive_service,
+            "1PyryHjH5Qi6fwkDrIejDUtW68Oc3V28R",
+            "input.csv",
+            const.DIR_TEMP)
+        df = pl.read_csv(fpath)
+    st.write(df)
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,47 @@
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseDownload
+
+import os
+import json
+
+
+def get_secrets():
+    """Secretsを取得"""
+    try:
+        print("Streamlit Cloud で実行しています")
+        gcp_creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+        google_genai_creds_info = service_account.Credentials.from_service_account_info(st.secrets["google_genai"])
+    except:
+        print("ローカル環境で実行しています")
+        with open("./.local/credentials.json") as f:
+            creds_info = json.load(f)
+        gcp_creds_info = creds_info["gcp"]
+        gcp_creds = service_account.Credentials.from_service_account_info(gcp_creds_info)
+
+        google_genai_creds_info = creds_info["google_genai"]
+    google_genai_api_key = google_genai_creds_info["api_key"]
+    return gcp_creds, google_genai_api_key
+
+
+def list_drive_files(drive_service):
+    """Google Drive のファイル一覧を取得"""
+    results = drive_service.files().list(pageSize=10, fields="files(id, name)").execute()
+    files = results.get("files", [])
+    return files
+
+
+def download_csv_file(drive_service, file_id, file_name, dir_path):
+    """Google Driveからファイルをダウンロード"""
+    request = drive_service.files().get_media(fileId=file_id)
+    
+    if os.path.isdir(dir_path) is False:
+        os.mkdir(dir_path)
+    file_path = os.path.join(dir_path, file_name)
+    with open(file_path, "wb") as f:
+        downloader = MediaIoBaseDownload(f, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}%.")
+    return file_path
+
